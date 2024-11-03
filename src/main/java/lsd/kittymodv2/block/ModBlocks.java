b8fff835bad48a68192ffc2a49884b28856c04c8
Alanik <lilia.dostie@socprep.org>
2024-11-03 14:44:10 -0500

    commit

--- src/main/java/lsd/kittymodv2/block/ModBlocks.java
+++ src/main/java/lsd/kittymodv2/block/ModBlocks.java
@@ -49,4 +49,8 @@ public class ModBlocks {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
 
-            entries
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+        });
+    }
+}
