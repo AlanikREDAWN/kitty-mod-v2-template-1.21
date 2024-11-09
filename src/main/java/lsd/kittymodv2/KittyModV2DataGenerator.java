d5d28f9c10c8456fd3b8f2a2a7404f7e9be7ba6c
Alanik <lilia.dostie@socprep.org>
2024-11-09 11:10:50 -0500

    more datagen setup

--- src/main/java/lsd/kittymodv2/KittyModV2DataGenerator.java
+++ src/main/java/lsd/kittymodv2/KittyModV2DataGenerator.java
@@ -1,9 +1,6 @@
 package lsd.kittymodv2;
 
-import lsd.kittymodv2.datagen.ModBlockTagProvider;
-import lsd.kittymodv2.datagen.ModItemTagProvider;
-import lsd.kittymodv2.datagen.ModLootTableProvider;
-import lsd.kittymodv2.datagen.ModModelProvider;
+import lsd.kittymodv2.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -16,5 +13,6 @@ public class KittyModV2DataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModItemTagProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
--- src/main/java/lsd/kittymodv2/datagen/ModRecipeProvider.java
+++ src/main/java/lsd/kittymodv2/datagen/ModRecipeProvider.java
@@ -1,10 +1,20 @@
 package lsd.kittymodv2.datagen;
 
+import lsd.kittymodv2.KittyModV2;
+import lsd.kittymodv2.block.ModBlocks;
+import lsd.kittymodv2.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -13,7 +23,66 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(RecipeExporter recipeExporter) {
-        
+    public void generate(RecipeExporter exporter) {
+
+        List<ItemConvertible> PINK_GARNET_SMELTABLES = List.of(ModItems.RAW_PINK_GARNET, ModBlocks.PINK_GARNET_ORE,
+                ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+
+        offerSmelting(exporter, PINK_GARNET_SMELTABLES, RecipeCategory.MISC, ModItems.PINK_GARNET, 0.25f, 200, "pink_garnet");
+        offerBlasting(exporter, PINK_GARNET_SMELTABLES, RecipeCategory.MISC, ModItems.PINK_GARNET, 0.25f, 100, "pink_garnet");
+
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.PINK_GARNET, RecipeCategory.DECORATIONS, ModBlocks.PINK_GARNET_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.RAW_PINK_GARNET_BLOCK)
+                .pattern("RRR")
+                .pattern("RRR")
+                .pattern("RRR")
+                .input('R', ModItems.RAW_PINK_GARNET)
+                .criterion(hasItem(ModItems.RAW_PINK_GARNET), conditionsFromItem(ModItems.RAW_PINK_GARNET))
+                .offerTo(exporter);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_PINK_GARNET, 9)
+                .input(ModBlocks.RAW_PINK_GARNET_BLOCK)
+                .criterion(hasItem(ModBlocks.RAW_PINK_GARNET_BLOCK), conditionsFromItem(ModBlocks.RAW_PINK_GARNET_BLOCK))
+                .offerTo(exporter);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_PINK_GARNET, 32)
+                .input(ModBlocks.MAGIC_BLOCK)
+                .criterion(hasItem(ModBlocks.RAW_PINK_GARNET_BLOCK), conditionsFromItem(ModBlocks.RAW_PINK_GARNET_BLOCK))
+                .offerTo(exporter, Identifier.of(KittyModV2.MOD_ID, "raw_pink_garnet_from_magic_block"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.MEOW_MUFFIN)
+                .pattern(" # ")
+                .pattern("WEM")
+                .pattern(" S ")
+                .input('#', Items.TROPICAL_FISH)
+                .input('W', Items.WHEAT)
+                .input('E', Items.EGG)
+                .input('M', Items.MILK_BUCKET)
+                .input('S', Items.SUGAR)
+                .criterion(hasItem(Items.TROPICAL_FISH), conditionsFromItem(Items.TROPICAL_FISH))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.ORANGE_COLLAR)
+                .pattern("#I#")
+                .pattern(" O ")
+                .pattern("#K#")
+                .input('#', Items.LEATHER)
+                .input('I', Items.IRON_NUGGET)
+                .input('O', Items.ORANGE_DYE)
+                .input('K', ModItems.CAT_BELL)
+                .criterion(hasItem(ModItems.CAT_BELL), conditionsFromItem(ModItems.CAT_BELL))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.PURPLE_COLLAR)
+                .pattern("#I#")
+                .pattern(" P ")
+                .pattern("#K#")
+                .input('#', Items.LEATHER)
+                .input('I', Items.IRON_NUGGET)
+                .input('P', Items.PURPLE_DYE)
+                .input('K', ModItems.CAT_BELL)
+                .criterion(hasItem(ModItems.CAT_BELL), conditionsFromItem(ModItems.CAT_BELL))
+                .offerTo(exporter);
     }
 }
